797e0e7908afbeb5b307093eb14ac02284149989
ame <93990980+amesupakorn@users.noreply.github.com>
2022-09-17 01:50:35 +0700

    Add files via upload

--- pair onliine week 3 16sep/ProgressiveTax.py
+++ pair onliine week 3 16sep/ProgressiveTax.py
@@ -2,20 +2,27 @@
 
 def main(money):
     """ ProgressiveTax """
-    if money <= 150000:
-        print(0)
-    elif money <= 300000:
-        print(int(((money - 150000)*0.05)))
-    elif money <= 500000:
-        print(int(((money - 300000)*0.10)) + 7500)
-    elif money <= 750000:
-        print(int(((money - 500000)*0.15)) + 27500)
-    elif money <= 1000000:
-        print(int(((money - 750000)*0.2)) + 65000)
-    elif money <= 2000000:
-        print(int(((money - 1000000)*0.25)) + 115000)
-    elif money <= 4000000:
-        print(int(((money - 2000000)*0.3)) + 365000)
-    else:
-        print(int(((money - 4000000)*0.35)) + 965000)
+    total = 0
+    if money > 4000000:
+        total += (money - 4000000) * 0.35 #หาว่า ต้องจ่ายภาษีเพิ่มเท่าไร
+        money -= (money - 4000000) #ทำการหักลบภาษีรอบนี้ เพื่อไปคิดรอบต่อไป
+    if money > 2000000:
+        total += (money - 2000000) * 0.30
+        money -= (money - 2000000)
+    if money > 1000000:
+        total += (money - 1000000) * 0.25
+        money -= (money - 1000000)
+    if money > 750000:
+        total += (money - 750000) * 0.2
+        money -= (money - 750000)
+    if money > 500000:
+        total += (money - 500000) * 0.15
+        money -= (money - 500000)
+    if money > 300000:
+        total += (money - 300000) * 0.1
+        money -= (money - 300000)
+    if money > 150000:
+        total += (money - 150000) * 0.05
+        money -= (money - 150000)
+    print("%d" %total)
 main(int(input()))

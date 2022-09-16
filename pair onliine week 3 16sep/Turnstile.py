e07021c26212bcb7a6f0fa0ef8a3b45bb7222ad9
ame <93990980+amesupakorn@users.noreply.github.com>
2022-09-17 00:46:12 +0700

    Add files via upload

--- pair onliine week 3 16sep/Turnstile.py
+++ pair onliine week 3 16sep/Turnstile.py
@@ -2,5 +2,20 @@
 
 def main():
     """ Turnstile """
-    
+    all_action = []
+    first = ""
+    people = 0
+    while True:
+        action = input()
+        if action == "END":
+            break
+        else:
+            all_action.append(action)
+    for i in all_action:
+        if i == "C":
+            first = "C"
+        if first + i == "CP":
+            people += 1
+            first = ""
+    print(people)
 main()
